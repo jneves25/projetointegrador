da64c08fc46e9c3ca53e9da36d577c0679f7610d
jneves25 <65085727+jneves25@users.noreply.github.com>
2020-09-25 14:37:52 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace Projeto_integrador
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textBoxID = new System.Windows.Forms.TextBox();
+            this.textBoxNome = new System.Windows.Forms.TextBox();
+            this.textBoxGenero = new System.Windows.Forms.TextBox();
+            this.textBoxAno = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.labelID = new System.Windows.Forms.Label();
+            this.labelNome = new System.Windows.Forms.Label();
+            this.labelGenero = new System.Windows.Forms.Label();
+            this.labelAno = new System.Windows.Forms.Label();
+            this.ColumnID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnNome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnGenero = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnAno = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.buttonSalvar = new System.Windows.Forms.Button();
+            this.buttonAtualizar = new System.Windows.Forms.Button();
+            this.buttonLimpar = new System.Windows.Forms.Button();
+            this.buttonDeletar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // textBoxID
+            // 
+            this.textBoxID.Enabled = false;
+            this.textBoxID.Location = new System.Drawing.Point(12, 30);
+            this.textBoxID.Name = "textBoxID";
+            this.textBoxID.Size = new System.Drawing.Size(31, 20);
+            this.textBoxID.TabIndex = 2;
+            // 
+            // textBoxNome
+            // 
+            this.textBoxNome.BackColor = System.Drawing.SystemColors.Window;
+            this.textBoxNome.Location = new System.Drawing.Point(50, 29);
+            this.textBoxNome.Name = "textBoxNome";
+            this.textBoxNome.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNome.TabIndex = 3;
+            // 
+            // textBoxGenero
+            // 
+            this.textBoxGenero.Location = new System.Drawing.Point(157, 29);
+            this.textBoxGenero.Name = "textBoxGenero";
+            this.textBoxGenero.Size = new System.Drawing.Size(100, 20);
+            this.textBoxGenero.TabIndex = 4;
+            // 
+            // textBoxAno
+            // 
+            this.textBoxAno.Location = new System.Drawing.Point(263, 29);
+            this.textBoxAno.Name = "textBoxAno";
+            this.textBoxAno.Size = new System.Drawing.Size(81, 20);
+            this.textBoxAno.TabIndex = 5;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnID,
+            this.ColumnNome,
+            this.ColumnGenero,
+            this.ColumnAno});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 119);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(402, 319);
+            this.dataGridView1.TabIndex = 6;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // labelID
+            // 
+            this.labelID.AutoSize = true;
+            this.labelID.Location = new System.Drawing.Point(9, 14);
+            this.labelID.Name = "labelID";
+            this.labelID.Size = new System.Drawing.Size(18, 13);
+            this.labelID.TabIndex = 7;
+            this.labelID.Text = "ID";
+            // 
+            // labelNome
+            // 
+            this.labelNome.AutoSize = true;
+            this.labelNome.Location = new System.Drawing.Point(50, 13);
+            this.labelNome.Name = "labelNome";
+            this.labelNome.Size = new System.Drawing.Size(35, 13);
+            this.labelNome.TabIndex = 8;
+            this.labelNome.Text = "Nome";
+            // 
+            // labelGenero
+            // 
+            this.labelGenero.AutoSize = true;
+            this.labelGenero.Location = new System.Drawing.Point(157, 14);
+            this.labelGenero.Name = "labelGenero";
+            this.labelGenero.Size = new System.Drawing.Size(42, 13);
+            this.labelGenero.TabIndex = 9;
+            this.labelGenero.Text = "Genero";
+            // 
+            // labelAno
+            // 
+            this.labelAno.AutoSize = true;
+            this.labelAno.Location = new System.Drawing.Point(264, 13);
+            this.labelAno.Name = "labelAno";
+            this.labelAno.Size = new System.Drawing.Size(26, 13);
+            this.labelAno.TabIndex = 10;
+            this.labelAno.Text = "Ano";
+            // 
+            // ColumnID
+            // 
+            this.ColumnID.HeaderText = "ID";
+            this.ColumnID.Name = "ColumnID";
+            // 
+            // ColumnNome
+            // 
+            this.ColumnNome.HeaderText = "Nome";
+            this.ColumnNome.Name = "ColumnNome";
+            // 
+            // ColumnGenero
+            // 
+            this.ColumnGenero.HeaderText = "Genero";
+            this.ColumnGenero.Name = "ColumnGenero";
+            // 
+            // ColumnAno
+            // 
+            this.ColumnAno.HeaderText = "Ano";
+            this.ColumnAno.Name = "ColumnAno";
+            // 
+            // buttonSalvar
+            // 
+            this.buttonSalvar.BackColor = System.Drawing.SystemColors.Control;
+            this.buttonSalvar.Location = new System.Drawing.Point(12, 79);
+            this.buttonSalvar.Name = "buttonSalvar";
+            this.buttonSalvar.Size = new System.Drawing.Size(75, 23);
+            this.buttonSalvar.TabIndex = 11;
+            this.buttonSalvar.Text = "Salvar";
+            this.buttonSalvar.UseVisualStyleBackColor = false;
+            this.buttonSalvar.Click += new System.EventHandler(this.buttonSalvar_Click);
+            // 
+            // buttonAtualizar
+            // 
+            this.buttonAtualizar.Location = new System.Drawing.Point(94, 78);
+            this.buttonAtualizar.Name = "buttonAtualizar";
+            this.buttonAtualizar.Size = new System.Drawing.Size(75, 23);
+            this.buttonAtualizar.TabIndex = 12;
+            this.buttonAtualizar.Text = "Atualizar";
+            this.buttonAtualizar.UseVisualStyleBackColor = true;
+            this.buttonAtualizar.Click += new System.EventHandler(this.buttonAtualizar_Click);
+            // 
+            // buttonLimpar
+            // 
+            this.buttonLimpar.Location = new System.Drawing.Point(175, 78);
+            this.buttonLimpar.Name = "buttonLimpar";
+            this.buttonLimpar.Size = new System.Drawing.Size(75, 23);
+            this.buttonLimpar.TabIndex = 13;
+            this.buttonLimpar.Text = "Limpar";
+            this.buttonLimpar.UseVisualStyleBackColor = true;
+            this.buttonLimpar.Click += new System.EventHandler(this.buttonLimpar_Click);
+            // 
+            // buttonDeletar
+            // 
+            this.buttonDeletar.Location = new System.Drawing.Point(257, 77);
+            this.buttonDeletar.Name = "buttonDeletar";
+            this.buttonDeletar.Size = new System.Drawing.Size(75, 23);
+            this.buttonDeletar.TabIndex = 14;
+            this.buttonDeletar.Text = "Deletar";
+            this.buttonDeletar.UseVisualStyleBackColor = true;
+            this.buttonDeletar.Click += new System.EventHandler(this.buttonDeletar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(443, 450);
+            this.Controls.Add(this.buttonDeletar);
+            this.Controls.Add(this.buttonLimpar);
+            this.Controls.Add(this.buttonAtualizar);
+            this.Controls.Add(this.buttonSalvar);
+            this.Controls.Add(this.labelAno);
+            this.Controls.Add(this.labelGenero);
+            this.Controls.Add(this.labelNome);
+            this.Controls.Add(this.labelID);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBoxAno);
+            this.Controls.Add(this.textBoxGenero);
+            this.Controls.Add(this.textBoxNome);
+            this.Controls.Add(this.textBoxID);
+            this.Name = "Form1";
+            this.Text = "Locadora";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox textBoxID;
+        private System.Windows.Forms.TextBox textBoxNome;
+        private System.Windows.Forms.TextBox textBoxGenero;
+        private System.Windows.Forms.TextBox textBoxAno;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnNome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnGenero;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnAno;
+        private System.Windows.Forms.Label labelID;
+        private System.Windows.Forms.Label labelNome;
+        private System.Windows.Forms.Label labelGenero;
+        private System.Windows.Forms.Label labelAno;
+        private System.Windows.Forms.Button buttonSalvar;
+        private System.Windows.Forms.Button buttonAtualizar;
+        private System.Windows.Forms.Button buttonLimpar;
+        private System.Windows.Forms.Button buttonDeletar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Projeto_integrador
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private
+            MySqlConnectionStringBuilder conexaoBanco()
+        {
+            MySqlConnectionStringBuilder conexaoBD = new MySqlConnectionStringBuilder();
+            conexaoBD.Server = "localhost";
+            conexaoBD.Database = "locadora";
+            conexaoBD.UserID = "root";
+            conexaoBD.Password = "";
+            return conexaoBD;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            atualizarGrid();
+        }
+
+        private void buttonSalvar_Click(object sender, EventArgs e)
+        {
+            MySqlConnectionStringBuilder conexaoBD = conexaoBanco();
+            MySqlConnection realizaConexacoBD = new MySqlConnection(conexaoBD.ToString()); try
+            {
+                realizaConexacoBD.Open();
+
+                MySqlCommand comandoMySql = realizaConexacoBD.CreateCommand();
+                comandoMySql.CommandText = "INSERT INTO filme (nomeFilme,generoFilme,anoFilme) " +
+                    "VALUES('" + textBoxNome.Text + "', '" + textBoxGenero.Text + "', " + Convert.ToInt16(textBoxAno.Text) + ")";
+                comandoMySql.ExecuteNonQuery();
+
+                realizaConexacoBD.Close();
+                MessageBox.Show("Inserido!");
+                limparCampos();
+                atualizarGrid();
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void buttonLimpar_Click(object sender, EventArgs e)
+        {
+            limparCampos();
+        }
+
+        private void limparCampos()
+        {
+            textBoxID.Clear();
+            textBoxAno.Clear();
+            textBoxGenero.Clear();
+            textBoxNome.Clear();
+        }
+
+        private void buttonAtualizar_Click(object sender, EventArgs e)
+        {
+            {
+                MySqlConnectionStringBuilder conexaoBD = conexaoBanco();
+                MySqlConnection realizaConexacoBD = new MySqlConnection(conexaoBD.ToString());
+                try
+                {
+                    realizaConexacoBD.Open(); 
+
+                    MySqlCommand comandoMySql = realizaConexacoBD.CreateCommand(); 
+                    comandoMySql.CommandText = "UPDATE filme SET nomeFilme = '" + textBoxNome.Text + "', " +
+                        "generoFilme = '" + textBoxGenero.Text + "', " +
+                        "anoFilme = '" + textBoxAno.Text + "' WHERE idFilme = " + textBoxID.Text + "";
+                    comandoMySql.ExecuteNonQuery();
+
+                    realizaConexacoBD.Close();
+                    MessageBox.Show("Atualizado!");
+                    atualizarGrid();
+                    limparCampos();
+                }
+                catch (Exception ex)
+                {
+                    
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value != null)
+            {
+                dataGridView1.CurrentRow.Selected = true;
+                
+                textBoxNome.Text = dataGridView1.Rows[e.RowIndex].Cells["ColumnNome"].FormattedValue.ToString();
+                textBoxGenero.Text = dataGridView1.Rows[e.RowIndex].Cells["ColumnGenero"].FormattedValue.ToString();
+                textBoxAno.Text = dataGridView1.Rows[e.RowIndex].Cells["ColumnAno"].FormattedValue.ToString();
+                textBoxID.Text = dataGridView1.Rows[e.RowIndex].Cells["ColumnID"].FormattedValue.ToString();
+            }
+        }
+
+            private void atualizarGrid()
+
+        {
+            MySqlConnectionStringBuilder conexaoBD = conexaoBanco();
+            MySqlConnection realizaConexacoBD = new MySqlConnection(conexaoBD.ToString());
+            try
+            {
+                realizaConexacoBD.Open();
+
+                MySqlCommand comandoMySql = realizaConexacoBD.CreateCommand();
+                comandoMySql.CommandText = "SELECT * FROM filme WHERE inativoFilme = 0";
+                MySqlDataReader reader = comandoMySql.ExecuteReader();
+
+                dataGridView1.Rows.Clear();
+
+                while (reader.Read())
+                {
+                    DataGridViewRow row = (DataGridViewRow)dataGridView1.Rows[0].Clone();
+                    row.Cells[0].Value = reader.GetInt32(0);
+                    row.Cells[1].Value = reader.GetString(1);
+                    row.Cells[3].Value = reader.GetString(2);
+                    row.Cells[2].Value = reader.GetString(3);
+                    dataGridView1.Rows.Add(row);
+                }
+
+                realizaConexacoBD.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Can not open connection ! ");
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void buttonDeletar_Click(object sender, EventArgs e)
+        {
+            MySqlConnectionStringBuilder conexaoBD = conexaoBanco();
+            MySqlConnection realizaConexacoBD = new MySqlConnection(conexaoBD.ToString());
+            try
+            {
+                realizaConexacoBD.Open(); 
+
+                MySqlCommand comandoMySql = realizaConexacoBD.CreateCommand(); 
+                
+                comandoMySql.CommandText = "UPDATE filme SET inativoFilme = 1 WHERE idFilme = " + textBoxID.Text + "";
+                comandoMySql.ExecuteNonQuery();
+
+                realizaConexacoBD.Close(); 
+                MessageBox.Show("Deletado!"); 
+                atualizarGrid();
+                limparCampos();
+            }
+            catch (Exception ex)
+            {
+                
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
